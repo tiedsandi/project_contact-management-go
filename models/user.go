32ebf698b4ca0550efdbe5002bbc477bd2fefa87
tiedsandi <fachransandi@gmail.com>
2025-06-25 22:41:22 +0700

    [FEATURE] implement user signup flow with password hashing and proper error handling

--- models/user.go
+++ models/user.go
@@ -1,10 +1,23 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/tiedsandi/project_contact-management-go/utils"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
 	Name     string `gorm:"not null"`
+}
+
+func (u *User) Save(db *gorm.DB) error {
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	u.Password = hashedPassword
+	return db.Create(u).Error
 }
--- routes/users.go
+++ routes/users.go
@@ -1,7 +1,28 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tiedsandi/project_contact-management-go/config"
+	"github.com/tiedsandi/project_contact-management-go/models"
+)
 
 func signup(context *gin.Context) {
+	var user models.User
 
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	err = user.Save(config.DB)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save data."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully."})
 }
