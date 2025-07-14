3c6f70bba17d36c3eea44d20a54c3f24d86dc083
tiedsandi <fachransandi@gmail.com>
2025-07-14 15:53:02 +0700

    [FEATURE] update dependencies in go.mod and go.sum; add CORS configuration in main.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"github.com/tiedsandi/project_contact-management-go/config"
@@ -13,6 +16,16 @@ func main() {
 	config.Migrate()
 
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		// AllowOrigins:     []string{"http://localhost:5173"},
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	routes.RegisterRoutes(server)
 
 	server.Run()
